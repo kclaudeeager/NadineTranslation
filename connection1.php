a1b66a5940bef173beec6f92aae31e92c7925e55
kclaudeeager <claudekwizera003@gmail.com>
2022-01-23 20:07:03 +0300

    Connecting to remote db

--- connection1.php
+++ connection1.php
@@ -1,21 +1,12 @@
 <?php
-
-$db = mysqli_connect("localhost","root","","translation");
-
-if(!$db)
-{
-    die("Connection failed: " . mysqli_connect_error());
-}
-//   $server = "localhost";
-//   $user ="root";
-//   $pass ="";
-//   $db = "translation";
-
-//   $conn = mysqli_connect($server, $user, $pass,$db);
-
-  //if($conn){
-	 // echo "Connected"."<br/>";
-  //}else{
-	 // echo "Not connected".mysqli_error($conn);
-  //}
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$db = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
